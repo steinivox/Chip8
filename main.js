9129b690315205cf8c6f756ea337c5db482078e5
Þorsteinn Vöggsson <steinivox@gmail.com>
2021-09-04 17:37:22 +0000

    Added game selector and game hints

--- main.js
+++ main.js
@@ -23,11 +23,43 @@ function title(str) {
     const game = window.location.search.replace("?", "") || "TETRIS";
     document.title += " - " + title(game);
     document.querySelector("h1").textContent += " - " + title(game);
-    const chip8 = new Chip8(await fetchGame(game));
+    new Chip8(await fetchGame(game));
 
     const canvas = document.querySelector("canvas");
     canvas.ondblclick = () => {
         if (!document.fullscreenElement) canvas.requestFullscreen();
         else document.exitFullscreen();
+    };
+
+    const gamesSel = document.querySelector("#games");
+    gamesSel.onchange = async e => {
+        const hints = {
+            "15PUZZLE": "",
+            "BLINKY": "",
+            "BLITZ": "W: drop bomb",
+            "BRIX": "Q: left, E: right",
+            "CONNECT4": "Q: left, E: right, W: drop",
+            "GUESS": "",
+            "HIDDEN": "Q: left, E: right, 2: up, S: down, W: select",
+            "INVADERS": "Q: left, E: right, W: shoot",
+            "KALEID": "Q: left, E: right, 2: up, S: down, W: toggle",
+            "MAZE": "",
+            "MERLIN": "Q W A S",
+            "MISSILE": "",
+            "PONG": "Player 1, 1: up, Q: down. Player 2: 4: up, R: down",
+            "PONG2": "Player 1, 1: up, Q1: down. Player 2: 4: up, R: down",
+            "PUZZLE": "Q: left, E: right, S: up, 2: down",
+            "SYZYGY": "",
+            "TANK": "Q: left, E: right, S: up, 2: down, W: shoot",
+            "TETRIS": "Q: rotate, W: left, E: right, A: down",
+            "TICTAC": "",
+            "UFO": "",
+            "VBRIX": "1: up, Q: down",
+            "VERS": "",
+            "WIPEOFF": "Q: left, E: right",
+        }
+        const  game = e.srcElement.value;
+        document.querySelector(".hints").textContent = hints[game];
+        new Chip8(await fetchGame(game));
     };
 })();
